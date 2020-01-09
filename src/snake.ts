116441861331b8652dd79c87dfe9fc3d01f11e27
fsiino <frankie.siino@gmail.com>
2020-01-08 23:58:08 -0800

    Add event listener to snake class

--- src/snake.ts
+++ src/snake.ts
@@ -4,6 +4,8 @@ const tune = {
   dY: 15,
 }
 
+type SnakeLoc = [number, number];
+
 export class Snake {
   private ctx : CanvasRenderingContext2D;
   private x: number;
@@ -14,8 +16,8 @@ export class Snake {
 
   private dir: string;
   
-  public head: number[];
-  public body: [number, number]
+  public head: number[]; // an array of numbers
+  public body: SnakeLoc[]; // 2d array of coordinates, e.g. [ [1,2], [1,3] ]
 
   constructor(canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext("2d");
@@ -26,33 +28,65 @@ export class Snake {
     this.snakePieceWidth = canvasWidth / 20;
     this.snakePieceHeight = canvasHeight / 20;
 
-    document.addEventListener("keydown", e => {
-      switch(e.key) {
-        case "Down" || "ArrowDown":
-          this.dir = "Down";
-        case "Up" || "ArrowUp":
-          this.dir = "Up";
-        case "Left" || "ArrowLeft":
-          this.dir = "Left";
-        case "Right" || "ArrowRight":
-          this.dir = "Right";
-      }
-    })
-    
-    
+    // document.addEventListener("keydown", e => {
+    //   switch(e.key) {
+    //     case "Down" || "ArrowDown":
+    //       this.dir = "Down";
+    //       console.log('down')
+    //       break;
+    //     case "Up" || "ArrowUp":
+    //       this.dir = "Up";
+    //       break;
+    //     case "Left" || "ArrowLeft":
+    //       this.dir = "Left";
+    //       break;
+    //     case "Right" || "ArrowRight":
+    //       this.dir = "Right";
+    //       break;
+    //     default: 
+    //       return;
+    //   }
+    // })
   }
 
   draw() {
     // for (let i = 0; i < this.body.length; i++) {
       
     // }
-    this.ctx.fillStyle = 'rgb(91, 20, 124)'
+    document.addEventListener("keydown", e => {
+      switch (e.key) {
+        case "ArrowDown":
+          this.dir = "Down";
+          console.log('down')
+          break;
+        case "ArrowUp":
+          this.dir = "Up";
+          console.log('up')
+          break;
+        case "ArrowLeft":
+          this.dir = "Left";
+          console.log('left')
+          break;
+        case "ArrowRight":
+          this.dir = "Right";
+          console.log('right')
+          break;
+        default:
+          break;
+      }
+    })
+    this.ctx.fillStyle = 'rgb(89, 0, 255)'
+    this.ctx.strokeStyle = '#000'
     this.ctx.fillRect(
-      250,
       50,
-      10,
-      10
+      50,
+      this.snakePieceWidth,
+      this.snakePieceHeight
     )
+  }
+
+  update() {
+    
   }
 
 }
