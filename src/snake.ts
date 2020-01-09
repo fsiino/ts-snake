ae9445404a4e4a763273cc96b4c739fc9a6263f4
fsiino <frankie.siino@gmail.com>
2020-01-08 16:57:28 -0800

    Add some styling

---
+++ src/snake.ts
@@ -0,0 +1,11 @@
+// type coords = [number, number];
+
+export class Snake {
+  private ctx : CanvasRenderingContext2D;
+  private x: number;
+  private y: number;
+  
+  private head: number[];
+  
+  private body: [number, number];
+}
