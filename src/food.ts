198e61b3e6f84741f70d4afb455ed5269699d244
fsiino <frankie.siino@gmail.com>
2020-01-15 17:21:31 -0800

    Start new Food class

--- src/food.ts
+++ src/food.ts
@@ -1,39 +1,23 @@
 export class Food {
-
   public ctx: CanvasRenderingContext2D;
-
-  public foodQueue: Array<[number, number]>
-  public firstServing: [number, number]
-
-  public canvasWidth: number;
-  public canvasHeight: number;
 
   constructor(canvas: HTMLCanvasElement) {
     this.ctx = canvas.getContext('2d');
 
-    this.canvasWidth = canvas.width;
-    this.canvasHeight = canvas.height;
 
-    
-    this.foodQueue = [];
-    this.getFoodLoc();
   }
 
-  public getFoodLoc() {
-    let randomCoordX = Math.floor(Math.random() * (this.canvasWidth - 10) + 1)
-    let randomCoordY = Math.floor(Math.random() * (this.canvasHeight - 10) + 1)
-    this.foodQueue.push([randomCoordX, randomCoordY]);
+  //TODO: Make tunable with constant
+  public randomFoods(min: number,max: number):number { 
+    let random = Math.round(Math.random() * (max-min) * 10);
+    return random;
   }
 
-  public spawn() {
-    for(let i = 0; i < this.foodQueue.length; i++) {
-      this.ctx.fillStyle = 'red';
-      this.ctx.fillRect(
-        this.foodQueue[i][0],
-        this.foodQueue[i][1],
-        8,
-        5
-      )
-    }
+  public createFood() {
+
+  }
+
+  public drawFood() {
+
   }
 }
--- src/snake.ts
+++ src/snake.ts
@@ -29,7 +29,7 @@ export class Snake {
     this.dy = 10;
       
     // document.addEventListener("keydown", this.turn.bind(this))
-    document.addEventListener('keydown', (e) => {
+    document.addEventListener('keydown', e => {
       this.turn(e)
     })
 
@@ -71,33 +71,34 @@ export class Snake {
     const UP = 87;
     const DOWN = 83;
 
+    //TODO: Fix 180 degree turn positioning e.g. (up -> left, down -> should shift down and not overlap with previous path)
     switch(e.keyCode) {
       case LEFT:
-        if (this.dx !== 10) {
+        if (this.dx !== 10) { // Disables 180 degree turn e.g. (right -> left)
           this.dx = -10;
           this.dy = 0;
-          console.log('l')
+          // console.log('l')
           break;
       }
       case RIGHT:
         if (this.dx !== -10) {
           this.dx = 10;
           this.dy = 0;
-          console.log('r')
+          // console.log('r')
           break;
       }
       case UP:
         if (this.dy !== 10) {
           this.dx = 0;
           this.dy = -10;
-          console.log('u')
+          // console.log('u')
           break;
       }
       case DOWN:
         if (this.dy !== -10) {
           this.dx = 0;
           this.dy = 10;
-          console.log('d')
+          // console.log('d')
           break;
       }
       default:
